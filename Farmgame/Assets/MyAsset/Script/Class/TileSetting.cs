fac3c46bfb4f5a5759243c9c257fbe141d04e33e
Newmoonhana <newmoonhana@naver.com>
2020-12-17 05:03:43 +0900

    프리팹 브러시로 밭 오브젝트 생성, 밭 오브젝트 내 스크립트 작성(미완성, 피드백 필요)

--- Farmgame/Assets/MyAsset/Script/Class/TileSetting.cs
+++ Farmgame/Assets/MyAsset/Script/Class/TileSetting.cs
@@ -4,23 +4,51 @@ using UnityEngine;
 
 public struct Tile
 {
+    enum TileMode
+    {
+        No_data,
+        Default_field
+    }
+
     public class TileSetting : MonoBehaviour
     {
-        int tilemode = 1;   //1: 평지, 2: 밭.
+        string tileobj_name = "";
+        TileMode tilemode = TileMode.Default_field;
         System.DateTime startTime; //작물을 심기 시작한 시간 저장.
         System.TimeSpan growthTime; //작물이 자라날 속도 시간 변수.
 
         //get set
-        public void SetGrowthTime(System.TimeSpan _time)
+        public string GetTileobjName()
         {
-            growthTime = _time;
+            return tileobj_name;
         }
-        public void SetGrowthTime(int _hours, int _minutes, int _seconds)
+        public void SetTileobjName(string _name)
         {
-            growthTime = new System.TimeSpan(_hours, _minutes, _seconds);
+            tileobj_name = _name;
+        }
+        public System.DateTime GetStartTime()
+        {
+            return startTime;
+        }
+        public void SetStartTime(int _hours, int _minutes)
+        {
+            startTime = System.DateTime.Now;
+            PlayerPrefs.SetInt(tileobj_name + "_startTime_dd", startTime.Day);
+            PlayerPrefs.SetInt(tileobj_name + "_startTime_hh", startTime.Hour);
+            PlayerPrefs.SetInt(tileobj_name + "_startTime_mm", startTime.Minute);
+
+        }
+        public System.TimeSpan GetGrowthTime()
+        {
+            return growthTime;
+        }
+        public void SetGrowthTime(int _minutes)
+        {
+            growthTime = new System.TimeSpan(_minutes / 60, _minutes % 60, 0);
+            PlayerPrefs.SetInt(tileobj_name + "_growthTime_mm", _minutes);
         }
 
-        bool GrowthTimeCheck()  //작물 성장 체크.
+        public bool GrowthTimeCheck()  //작물 성장 체크.
         {
             System.DateTime nowTime = System.DateTime.Now;
 
--- Farmgame/Assets/MyAsset/Script/Manager/GameManager.cs
+++ Farmgame/Assets/MyAsset/Script/Manager/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    GameManager inst;
+    public static GameManager inst;
+    public PlayerPrefsManager pp_mng;
 
-    private void Awake()
+    void Awake()
     {
+        if (inst != null)   //간단한 싱글톤.
+        {
+            Destroy(gameObject);
+            return;
+        }
         inst = this;
+        DontDestroyOnLoad(this);    //씬 이동 후 오브젝트 보존.
+
+        pp_mng.Load();
     }
 }
