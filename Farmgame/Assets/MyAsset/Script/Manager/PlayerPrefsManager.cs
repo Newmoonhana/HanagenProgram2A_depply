eccd28fcc74460ea5d381c3b8fd65247ed7c1ecd
school501 <magotoolivesource@gmail.com>
2020-12-22 09:37:14 +0900

    팜게임 삭제된것 복구 필요없는 파일 지우기

---
+++ Farmgame/Assets/MyAsset/Script/Manager/PlayerPrefsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    public InputField valOpen;
+    public bool reset = false;
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("Open", int.Parse(valOpen.text));
+    }
+
+    public void Load()
+    {
+        if (!PlayerPrefs.HasKey("Open"))
+        {
+            ResetPP();
+        }
+        valOpen.text = PlayerPrefs.GetInt("Open").ToString();
+    }
+
+    //모든 값 리셋.
+    public void ResetPP()
+    {
+        PlayerPrefs.DeleteAll();
+
+        PlayerPrefs.SetInt("Open", 0);
+
+
+        reset = false;
+    }
+}
